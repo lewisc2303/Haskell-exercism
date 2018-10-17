9e2605ad49b053429fd6d8ceffd6495b3b8c1e29
Lewis <lewiscoleman1995@hotmail.com>
2018-10-17 16:06:42 +0200

    add acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,12 +2,13 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
---I want to map a funciton that looks for capital letter into the [Char]
-
 abbreviate :: String -> String
-abbreviate word = go word []
-    where go word acr
+abbreviate word = go word [] ' '
+    where go word acronym lstChar
             | length word == 0 = ""
-            | isUpper (head word) == True = acr ++ ([head word]) ++ (go (tail word) acr)
-            | otherwise = go (tail word) acr
-                where wordHead = head word
+            | (isUpper lstChar) && (isUpper currentChar) == True = go restOfWord acronym currentChar
+            | ((lstChar == ' ') || (lstChar == '-')) == True = acronym ++ ([toUpper currentChar]) ++ (go restOfWord acronym currentChar)            
+            | isUpper currentChar == True = acronym ++ ([head word]) ++ (go restOfWord acronym currentChar)
+            | otherwise = go restOfWord acronym currentChar
+                where currentChar = head word
+                      restOfWord = tail word
